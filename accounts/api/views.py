cfe786a3e341e2ed09b5cd988493f379103be169
linr1 <linrui@gmail.com>
2021-04-30 21:17:02 -0700

    Optimized code

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -20,7 +20,8 @@ class UserViewSet(viewsets.ModelViewSet):
     API endpoint that allows users to be
     viewed or edited:
     """
-    queryset = User.objects.all().order_by("date_joined")
+    #queryset = User.objects.all().order_by("date_joined")
+    queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
