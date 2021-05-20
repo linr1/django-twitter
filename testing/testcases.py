8ab0ff430da43937cbe2e7f9ae0f6e4095ec6e00
linr1 <linrui@gmail.com>
2021-05-19 22:29:37 -0700

    Added likes model and admin

--- testing/testcases.py
+++ testing/testcases.py
@@ -1,8 +1,10 @@
 from django.test import TestCase as DjangoTestCase
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
 from tweets.models import Tweet
 from comments.models import Comment
 from newsfeeds.models import NewsFeed
+from likes.models import Like
 from rest_framework.test import APIClient
 
 
@@ -34,3 +36,11 @@ class TestCase(DjangoTestCase):
         if content is None:
             content = 'default comment content'
         return Comment.objects.create(user=user, tweet=tweet, content=content)
+
+    def create_like(self, user, target):
+        instance, _ = Like.objects.get_or_create(
+            content_type=ContentType.objects.get_for_model(target.__class__),
+            object_id=target.id,
+            user=user,
+        )
+        return instance
--- twitter/settings.py
+++ twitter/settings.py
@@ -47,7 +47,8 @@ INSTALLED_APPS = [
     'tweets',
     'friendships',
     'newsfeeds',
-    'comments'
+    'comments',
+    'likes',
 ]
 
 REST_FRAMEWORK = {
