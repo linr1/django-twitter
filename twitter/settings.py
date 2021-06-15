a50272d0ecedac3e9e423b2bd1969cfb1cb628ee
linr1 <linrui@gmail.com>
2021-06-14 22:04:19 -0700

    Install notifications & added to api

--- twitter/settings.py
+++ twitter/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'debug_toolbar',
+    'notifications',
 
     # project apps
     'tweets',
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
     'newsfeeds',
     'comments',
     'likes',
+    'inbox',
 ]
 
 REST_FRAMEWORK = {
